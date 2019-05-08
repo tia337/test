2735b64caea3e5f33e07cadcf17865bf07d96d45
Illia Telefus <ilya.it337@gmail.com>
2019-05-08 19:54:28 +0300

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 require('dotenv').load()
 
 const pgp = require('pg-promise')({});
-const conString = "postgres://jwzfdxnm:CndUYZwKDcRrBUXoCLlKWg2v4YMW7uYA@stampy.db.elephantsql.com:5432/jwzfdxnm"
+const conString = "yourDB"
 const DB = pgp(conString);
 
 module.exports = DB;
+//for test purposes you can use postgres://jwzfdxnm:CndUYZwKDcRrBUXoCLlKWg2v4YMW7uYA@stampy.db.elephantsql.com:5432/jwzfdxnm
